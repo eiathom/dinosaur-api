326beeafa3b9ec84a87815343e7cad0770e09363
eiathom <eiathom@protonmail.ch>
2024-01-14 13:44:19 +0000

    basic use cases for length parser defined

---
+++ app/test/unit/util/test_parse_utils.py
@@ -0,0 +1,11 @@
+def test_dinosaur_length_parser_returns_none_if_non_numerical_value_is_passed():
+    pass
+
+def test_dinosaur_length_parser_returns_float_if_integer_value_as_string_is_passed():
+    pass
+
+def test_dinosaur_length_parser_returns_float_if_float_value_as_string_is_passed():
+    pass
+
+def test_dinosaur_length_parser_returns_float_if_valid_partial_numerical_value_as_string_is_passed():
+    pass
