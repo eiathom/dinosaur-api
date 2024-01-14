f9a9abecb11e372d7fc8a7425a1722b1fc8e8f48
eiathom <eiathom@protonmail.ch>
2024-01-14 18:57:06 +0000

    adds function to get name with maximum length

---
+++ app/src/service/dinosaur.py
@@ -0,0 +1,21 @@
+from typing import List
+from pandas import DataFrame
+
+
+def get_dinosaur_names_according_to_maximum_length_of_dinosaur(
+    data_frame: DataFrame,
+    name_column_name: str,
+    length_column_name: str,
+) -> List[str]:
+    if data_frame is not None:
+        try:
+            max_length = data_frame[length_column_name].max()
+            return data_frame[data_frame[length_column_name] == max_length][
+                name_column_name
+            ].tolist()
+        except Exception as exception:
+            # TODO: decide on logging library
+            print(
+                f"unable to find dinosaur names given name column: '{name_column_name}' and length column: '{length_column_name}' because of: '{str(exception)}"
+            )
+    return []
